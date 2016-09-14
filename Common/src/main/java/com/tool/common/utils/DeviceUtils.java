5a8502f2b7614d8e2be25943123e049af0260087
DesignQu <250533855@qq.com>
2016-09-14 18:10:33 +0800

    初始导入

--- Common/src/main/java/com/tool/common/utils/DeviceUtils.java
+++ Common/src/main/java/com/tool/common/utils/DeviceUtils.java
@@ -44,25 +44,12 @@ public class DeviceUtils {
     }
 
     /**
-     * dp转px
+     * 判断是否存在sd卡
      *
-     * @param context
-     * @param dp
      * @return
      */
-    public static float dpToPixel(Context context, float dp) {
-        return dp * (getDisplayMetrics(context).densityDpi / 160F);
-    }
-
-    /**
-     * px转dp
-     *
-     * @param context
-     * @param f
-     * @return
-     */
-    public static float pixelsToDp(Context context, float f) {
-        return f / (getDisplayMetrics(context).densityDpi / 160F);
+    public static boolean isExitsSdcard() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
 
     /**
@@ -86,17 +73,25 @@ public class DeviceUtils {
     }
 
     /**
-     * 获取DisplayMetrics
+     * dp转px
      *
      * @param context
+     * @param dp
      * @return
      */
-    private static DisplayMetrics getDisplayMetrics(Context context) {
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        ((WindowManager) context.getSystemService(
-                Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(
-                displaymetrics);
-        return displaymetrics;
+    public static float dpToPixel(Context context, float dp) {
+        return dp * (getDisplayMetrics(context).densityDpi / 160F);
+    }
+
+    /**
+     * px转dp
+     *
+     * @param context
+     * @param f
+     * @return
+     */
+    public static float pixelsToDp(Context context, float f) {
+        return f / (getDisplayMetrics(context).densityDpi / 160F);
     }
 
     /**
@@ -115,12 +110,23 @@ public class DeviceUtils {
             obj = c.newInstance();
             field = c.getField("status_bar_height");
             x = Integer.parseInt(field.get(obj).toString());
-            return context.getResources()
-                    .getDimensionPixelSize(x);
+            return context.getResources().getDimensionPixelSize(x);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    /**
+     * 获取DisplayMetrics
+     *
+     * @param context
+     * @return
+     */
+    private static DisplayMetrics getDisplayMetrics(Context context) {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displaymetrics);
+        return displaymetrics;
     }
 
     /**
@@ -140,7 +146,7 @@ public class DeviceUtils {
         screenWidth = metrics.widthPixels;
         screenHeight = metrics.heightPixels;
         // includes window decorations (statusbar bar/menu bar)
-        if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 17)
+        if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 17) {
             try {
                 screenWidth = (Integer) Display.class.getMethod("getRawWidth")
                         .invoke(d);
@@ -148,8 +154,9 @@ public class DeviceUtils {
                         .getMethod("getRawHeight").invoke(d);
             } catch (Exception ignored) {
             }
+        }
         // includes window decorations (statusbar bar/menu bar)
-        if (Build.VERSION.SDK_INT >= 17)
+        if (Build.VERSION.SDK_INT >= 17) {
             try {
                 Point realSize = new Point();
                 Display.class.getMethod("getRealSize", Point.class).invoke(d,
@@ -158,6 +165,7 @@ public class DeviceUtils {
                 screenHeight = realSize.y;
             } catch (Exception ignored) {
             }
+        }
         size[0] = screenWidth;
         size[1] = screenHeight;
         return size;
@@ -171,10 +179,11 @@ public class DeviceUtils {
      */
     public static boolean isLandscape(Context context) {
         boolean flag;
-        if (context.getResources().getConfiguration().orientation == 2)
+        if (context.getResources().getConfiguration().orientation == 2) {
             flag = true;
-        else
+        } else {
             flag = false;
+        }
         return flag;
     }
 
@@ -186,8 +195,9 @@ public class DeviceUtils {
      */
     public static boolean isPortrait(Context context) {
         boolean flag = true;
-        if (context.getResources().getConfiguration().orientation != 1)
+        if (context.getResources().getConfiguration().orientation != 1) {
             flag = false;
+        }
         return flag;
     }
 
@@ -198,17 +208,34 @@ public class DeviceUtils {
      * @param view
      */
     public static void hideSoftKeyboard(Context context, View view) {
-        if (view == null)
+        if (view == null) {
             return;
-        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(
-                Context.INPUT_METHOD_SERVICE);
-        if (inputMethodManager.isActive())
-            inputMethodManager.hideSoftInputFromWindow(
-                    view.getWindowToken(), 0);
+        }
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager.isActive()) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
     /**
-     * 当前的包是否存在
+     * 当前是否有网络
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasInternet(Context context) {
+        boolean flag;
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (manager != null && manager.getActiveNetworkInfo() != null) {
+            flag = true;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    /**
+     * 判断包是否存在
      *
      * @param context
      * @param pckName
@@ -216,12 +243,12 @@ public class DeviceUtils {
      */
     public static boolean isPackageExist(Context context, String pckName) {
         try {
-            PackageInfo pckInfo = context.getPackageManager()
-                    .getPackageInfo(pckName, 0);
-            if (pckInfo != null)
+            PackageInfo pckInfo = context.getPackageManager().getPackageInfo(pckName, 0);
+            if (pckInfo != null) {
                 return true;
+            }
         } catch (PackageManager.NameNotFoundException e) {
-            Log.e("TDvice", e.getMessage());
+            Log.e("", e.getMessage());
         }
         return false;
     }
@@ -235,48 +262,102 @@ public class DeviceUtils {
     public static void uninstallApk(Context context, String packageName) {
         if (isPackageExist(context, packageName)) {
             Uri packageURI = Uri.parse("package:" + packageName);
-            Intent uninstallIntent = new Intent(Intent.ACTION_DELETE,
-                    packageURI);
+            Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageURI);
             context.startActivity(uninstallIntent);
         }
     }
 
     /**
-     * 判断是否存在sd卡
+     * 设备是否有后置or前置相机
      *
+     * @param context
+     * @param flag    后置or前置
      * @return
      */
-    public static boolean isExitsSdcard() {
-        if (Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED))
-            return true;
-        else
-            return false;
+    public static boolean hasCamera(Context context, boolean flag) {
+        boolean hasCamera = false;
+        PackageManager pckMgr = context.getPackageManager();
+        if (flag) {
+            hasCamera = pckMgr.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+        } else {
+            hasCamera = pckMgr.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        }
+        return hasCamera;
     }
 
-//    /**
-//     * 设备是否有相机
-//     *
-//     * @param context
-//     * @return
-//     */
-//    public static final boolean hasCamera(Context context) {
-//        if (_hasCamera == null) {
-//            PackageManager pckMgr = context
-//                    .getPackageManager();
-//            boolean flag = pckMgr
-//                    .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
-//            boolean flag1 = pckMgr.hasSystemFeature(PackageManager.FEATURE_CAMERA);
-//            boolean flag2;
-//            if (flag || flag1)
-//                flag2 = true;
-//            else
-//                flag2 = false;
-//            _hasCamera = Boolean.valueOf(flag2);
-//        }
-//
-//        return _hasCamera.booleanValue();
-//    }
+    /**
+     * 调用系统安装了的应用分享
+     *
+     * @param context
+     * @param title
+     * @param url
+     */
+    public static void showSystemShareOption(Activity context, final String title, final String url) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享：" + title);
+        intent.putExtra(Intent.EXTRA_TEXT, title + " " + url);
+        context.startActivity(Intent.createChooser(intent, "选择分享"));
+    }
+
+    /**
+     * 获取设备IMEI
+     *
+     * @param context
+     */
+    public static String getIMEI(Context context) {
+        return ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+    }
+
+    /**
+     * 获取版本号
+     *
+     * @param context
+     * @return
+     */
+    public static int getVersionCode(Context context) {
+        int versionCode = 0;
+        try {
+            versionCode = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException ex) {
+            versionCode = 0;
+        }
+        return versionCode;
+    }
+
+    /**
+     * 获取指定包名应用的版本号
+     *
+     * @param context
+     * @param packageName
+     * @return
+     */
+    public static int getVersionCode(Context context, String packageName) {
+        int versionCode = 0;
+        try {
+            versionCode = context.getPackageManager().getPackageInfo(packageName, 0).versionCode;
+        } catch (PackageManager.NameNotFoundException ex) {
+            versionCode = 0;
+        }
+        return versionCode;
+    }
+
+    /**
+     * 获取版本名
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        String name = "";
+        try {
+            name = context.getPackageManager().getPackageInfo(context.getPackageName(),
+                            0).versionName;
+        } catch (PackageManager.NameNotFoundException ex) {
+            name = "";
+        }
+        return name;
+    }
 }
 
 
