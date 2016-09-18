224e8e35efc3a936ff39bd76cfd1db6c452a2762
DesignQu <250533855@qq.com>
2016-09-18 14:43:26 +0800

    初始导入

--- app/src/main/java/com/frame/mvp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/frame/mvp/ui/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.frame.mvp;
+package com.frame.mvp.ui.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.frame.mvp.R;
 
 import java.util.ArrayList;
 import java.util.List;
