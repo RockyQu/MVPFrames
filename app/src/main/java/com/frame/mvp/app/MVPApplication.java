3956b2a7ff59635edb79f76ebda1fabe832a60ed
DesignQu <250533855@qq.com>
2016-09-18 15:53:15 +0800

    初始导入

--- app/src/main/java/com/frame/mvp/app/MVPApplication.java
+++ app/src/main/java/com/frame/mvp/app/MVPApplication.java
@@ -1,25 +1,6 @@
 package com.frame.mvp.app;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.jess.arms.base.BaseApplication;
-import com.jess.arms.http.GlobeHttpResultHandler;
-import com.jess.arms.utils.UiUtils;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import me.jessyan.mvparms.demo.di.component.AppComponent;
-import me.jessyan.mvparms.demo.di.component.DaggerAppComponent;
-import me.jessyan.mvparms.demo.di.module.CacheModule;
-import me.jessyan.mvparms.demo.di.module.ServiceModule;
-import me.jessyan.mvparms.demo.mvp.model.api.Api;
-import me.jessyan.rxerrorhandler.handler.listener.ResponseErroListener;
-import okhttp3.Interceptor;
-import okhttp3.Response;
-import timber.log.Timber;
 
 /**
  *
