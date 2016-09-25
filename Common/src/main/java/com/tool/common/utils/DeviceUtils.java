20228502750c5b24f4ffd07acceb540261926565
Qu <250533855@qq.com>
2016-09-25 13:37:26 +0800

    初始导入

--- Common/src/main/java/com/tool/common/utils/DeviceUtils.java
+++ Common/src/main/java/com/tool/common/utils/DeviceUtils.java
@@ -1,6 +1,7 @@
 package com.tool.common.utils;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -8,10 +9,12 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
@@ -259,6 +262,142 @@ public class DeviceUtils extends BaseUtils {
      */
     public static int getSDKVersion() {
         return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * 设置给定Activity的窗口的亮度（可以看到效果，但系统的亮度属性不会改变）
+     *
+     * @param activity         要通过此Activity来设置窗口的亮度
+     * @param screenBrightness 亮度，范围是0-255
+     */
+    public static void setWindowBrightness(Activity activity, float screenBrightness) {
+        float brightness = screenBrightness;
+        if (screenBrightness < 1) {
+            brightness = 1;
+        } else if (screenBrightness > 255) {
+            brightness = screenBrightness % 255;
+            if (brightness == 0) {
+                brightness = 255;
+            }
+        }
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams localLayoutParams = window.getAttributes();
+        localLayoutParams.screenBrightness = (float) brightness / 255;
+        window.setAttributes(localLayoutParams);
+    }
+
+    /**
+     * 设置系统亮度并实时可以看到效果，需要WRITE_SETTINGS权限
+     *
+     * @param activity         要通过此Activity来设置窗口的亮度
+     * @param screenBrightness 亮度，范围是0-255
+     * @return 设置是否成功
+     */
+    public static boolean setScreenBrightnessAndApply(Activity activity, int screenBrightness) {
+        boolean result = true;
+        result = setScreenBrightness(activity, screenBrightness);
+        if (result) {
+            setWindowBrightness(activity, screenBrightness);
+        }
+        return result;
+    }
+
+    /**
+     * 设置系统屏幕亮度模式，需要WRITE_SETTINGS权限
+     *
+     * @param context 上下文
+     * @param auto    自动
+     * @return 是否设置成功
+     */
+    public static boolean setScreenBrightnessMode(Context context, boolean auto) {
+        boolean result = true;
+        if (isScreenBrightnessModeAuto(context) != auto) {
+            result = Settings.System.putInt(context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    auto ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
+                            : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        }
+        return result;
+    }
+
+    /**
+     * 判断系统屏幕亮度模式是否是自动，需要WRITE_SETTINGS权限
+     *
+     * @param context 上下文
+     * @return true：自动；false：手动；默认：true
+     */
+    public static boolean isScreenBrightnessModeAuto(Context context) {
+        return getScreenBrightnessModeState(context) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC ? true
+                : false;
+    }
+
+    /**
+     * 获取系统屏幕亮度模式的状态，需要WRITE_SETTINGS权限
+     *
+     * @param context 上下文
+     * @return System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC：自动；System.
+     * SCREEN_BRIGHTNESS_MODE_AUTOMATIC
+     * ：手动；默认：System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
+     */
+    public static int getScreenBrightnessModeState(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+    }
+
+    /**
+     * 获取蓝牙的状态
+     *
+     * @return 取值为BluetoothAdapter的四个静态字段：STATE_OFF, STATE_TURNING_OFF,
+     * STATE_ON, STATE_TURNING_ON
+     * @throws Exception 没有找到蓝牙设备
+     */
+    public static int getBluetoothState() throws Exception {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter
+                .getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            throw new Exception("bluetooth device not found!");
+        } else {
+            return bluetoothAdapter.getState();
+        }
+    }
+
+    /**
+     * 判断蓝牙是否打开
+     *
+     * @return true：已经打开或者正在打开；false：已经关闭或者正在关闭
+     * 没有找到蓝牙设备
+     */
+    public static boolean isBluetoothOpen() {
+        int bluetoothStateCode = 0;
+        try {
+            bluetoothStateCode = getBluetoothState();
+            return bluetoothStateCode == BluetoothAdapter.STATE_ON
+                    || bluetoothStateCode == BluetoothAdapter.STATE_TURNING_ON ? true
+                    : false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 设置蓝牙状态
+     *
+     * @param enable 打开
+     *               没有找到蓝牙设备
+     */
+    public static void setBluetooth(boolean enable) {
+        // 如果当前蓝牙的状态与要设置的状态不一样
+        if (isBluetoothOpen() != enable) {
+            // 如果是要打开就打开，否则关闭
+            if (enable) {
+                BluetoothAdapter.getDefaultAdapter().enable();
+            } else {
+                BluetoothAdapter.getDefaultAdapter().disable();
+            }
+        }
+
     }
 }
 
