527d65b1a1a312b88096660c53fd9d1d9f91f78d
DesignQu <250533855@qq.com>
2016-10-14 14:34:54 +0800

    初始导入

---
+++ Common/src/main/java/com/tool/common/http/BaseCacheManager.java
@@ -0,0 +1,8 @@
+package com.tool.common.http;
+
+/**
+ * 缓存管理类，管理本地或内存缓存
+ */
+public abstract class BaseCacheManager {
+
+}
---
+++ Common/src/main/java/com/tool/common/http/BaseServiceManager.java
@@ -0,0 +1,8 @@
+package com.tool.common.http;
+
+/**
+ * 服务管理类
+ */
+public abstract class BaseServiceManager {
+
+}
