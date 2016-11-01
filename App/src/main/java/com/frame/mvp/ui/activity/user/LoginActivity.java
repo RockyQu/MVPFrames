da764707d0695301e87b8e5ada02cb0806426f3d
DesignQu <250533855@qq.com>
2016-11-01 15:00:46 +0800

    初始导入

--- App/src/main/java/com/frame/mvp/ui/activity/user/LoginActivity.java
+++ App/src/main/java/com/frame/mvp/ui/activity/user/LoginActivity.java
@@ -12,23 +12,24 @@ import com.frame.mvp.presenter.user.LoginPresenter;
 import com.frame.mvp.ui.common.CommonActivity;
 import com.tool.common.utils.AppUtils;
 
+import butterknife.BindView;
+
 /**
  * 登录页面
  */
 public class LoginActivity extends CommonActivity<LoginPresenter> implements Contract.View {
 
     // 账号
-    private EditText edtAccount = null;
+    @BindView(R.id.edt_account)
+    EditText edtAccount = null;
     // 密码
-    private EditText edtPassword = null;
+    @BindView(R.id.edt_password)
+    EditText edtPassword = null;
 
     @Override
     public void create(Bundle savedInstanceState) {
 
 //        presenter =
-
-        edtAccount = (EditText) findViewById(R.id.edt_account);
-        edtPassword = (EditText) findViewById(R.id.edt_password);
 
         Button btnSubmit = (Button) findViewById(R.id.btn_submit);
         btnSubmit.setOnClickListener(new OnClickListener() {
