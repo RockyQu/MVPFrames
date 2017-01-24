72052e0aa18f4da5db0c4464aa1101288784e9fe
DesignQu <250533855@qq.com>
2017-01-24 18:43:52 +0800

    初始导入

--- Common/src/main/java/com/tool/common/http/interceptor/NetworkInterceptor.java
+++ Common/src/main/java/com/tool/common/http/interceptor/NetworkInterceptor.java
@@ -105,4 +105,29 @@ public class NetworkInterceptor implements Interceptor {
 
         return originalResponse;
     }
+
+    /**
+     * Http通信拦截器
+     */
+    public interface NetworkHandler {
+
+        /**
+         * Http请求
+         *
+         * @param chain
+         * @param request
+         * @return Request
+         */
+        Request onHttpRequest(Interceptor.Chain chain, Request request);
+
+        /**
+         * Http响应，这里提前拿到http响应结果,可以用来判断Token是否过期
+         *
+         * @param result
+         * @param chain
+         * @param response
+         * @return Response
+         */
+        Response onHttpResponse(String result, Interceptor.Chain chain, Response response);
+    }
 }
