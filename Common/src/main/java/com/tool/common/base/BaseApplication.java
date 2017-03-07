71f25275590d4d7687ab48f64b48a61966225eef
DesignQu <250533855@qq.com>
2017-03-07 14:41:10 +0800

    无

--- Common/src/main/java/com/tool/common/base/BaseApplication.java
+++ Common/src/main/java/com/tool/common/base/BaseApplication.java
@@ -9,18 +9,26 @@ import com.tool.common.log.log.LogConfig;
 import com.tool.common.di.module.ImageModule;
 
 /**
- * 项目涉及的主要框架
+ * 一、项目涉及的主要框架
  * 1、Dagger2 https://google.github.io/dagger
  * 2、ButterKnife http://jakewharton.github.io/butterknife
  * 3、Retrofit2 https://github.com/square/retrofit
  * 4、Glide https://github.com/bumptech/glide
  * 5、Gson https://github.com/google/gson
  * <p>
- * 基本使用方法
+ * 二、基本使用方法
  * 1、配置自定义Application必须继承BaseApplication，BaseApplication完成Http框架、图片框架、日志管理框架等初始化工作
  * 2、使用Activity、Fragment、ViewHolder、Service、Adapter请继承CommonActivity、CommonFragment、BaseViewHolder、BaseService、BaseAdapter来初始化MVP架构
  * 3、通过Application获取AppComponent里面的对象可直接使用
  * 4、简单功能及页面无需引入MVP
+ * <p>
+ * 三、未来可能会更新的一些功能
+ * 1、路由框架
+ * 2、用户行为分析日志模块
+ * 3、优化MVP缺点（类、接口过多的问题）
+ * 4、重构蓝牙定位模块
+ * 5、数据库模块
+ * 6、混淆
  */
 public abstract class BaseApplication extends Application {
 
