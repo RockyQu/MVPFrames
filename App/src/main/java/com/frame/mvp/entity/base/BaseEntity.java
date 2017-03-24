535ac41ee81410a6bbf493c37430ed1b5ea22d4f
DesignQu <250533855@qq.com>
2017-03-24 13:27:54 +0800

    无

--- App/src/main/java/com/frame/mvp/entity/base/BaseEntity.java
+++ App/src/main/java/com/frame/mvp/entity/base/BaseEntity.java
@@ -1,14 +1,21 @@
 package com.frame.mvp.entity.base;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
- *
+ * BaseEntity
  */
 public interface BaseEntity {
 
-    class Bean {
+    class Bean implements Parcelable {
 
         // ID
         private int id;
+
+        public Bean() {
+            ;
+        }
 
         public int getId() {
             return id;
@@ -16,6 +23,33 @@ public interface BaseEntity {
 
         public void setId(int id) {
             this.id = id;
+        }
+
+        public Bean(Parcel source) {
+            this.id = source.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeInt(id);
+        }
+
+        public static final Creator<Bean> CREATOR = new Creator<Bean>() {
+
+            @Override
+            public Bean createFromParcel(Parcel source) {
+                return new Bean(source);
+            }
+
+            @Override
+            public Bean[] newArray(int size) {
+                return new Bean[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
         }
     }
 }
