4d5d9445ab67c8b7eb689118be02289b7896dd1d
DesignQu <250533855@qq.com>
2017-04-12 10:43:29 +0800

    无

--- App/src/main/java/com/frame/mvp/di/common/module/DBModule.java
+++ App/src/main/java/com/frame/mvp/di/common/module/DBModule.java
@@ -1,8 +1,14 @@
 package com.frame.mvp.di.common.module;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 import com.frame.mvp.entity.DaoMaster;
 import com.frame.mvp.entity.DaoSession;
+import com.frame.mvp.entity.UserDao;
 import com.tool.common.db.DBContextWrapper;
+import com.tool.common.db.MigrationHelper;
 import com.tool.common.utils.ProjectUtils;
 
 import org.greenrobot.greendao.query.QueryBuilder;
@@ -20,13 +26,13 @@ public class DBModule {
 
     @Singleton
     @Provides
-    public DaoMaster.DevOpenHelper provideDevOpenHelper() {
-        return new DaoMaster.DevOpenHelper(new DBContextWrapper(), ProjectUtils.PROJECT_NAME + ".db", null);
+    public DBOpenHelper provideDevOpenHelper() {
+        return new DBOpenHelper(new DBContextWrapper(), ProjectUtils.PROJECT_NAME + ".db", null);
     }
 
     @Singleton
     @Provides
-    public DaoMaster provideDaoMaster(DaoMaster.DevOpenHelper devOpenHelper) {
+    public DaoMaster provideDaoMaster(DBOpenHelper devOpenHelper) {
         return new DaoMaster(devOpenHelper.getWritableDb());
     }
 
@@ -36,12 +42,28 @@ public class DBModule {
         return daoMaster.newSession();
     }
 
-    //输出日志
-    public void setDebug() {
-        QueryBuilder.LOG_SQL = true;
-        QueryBuilder.LOG_VALUES = true;
+    /**
+     * 数据库升级模块
+     */
+    public class DBOpenHelper extends DaoMaster.OpenHelper {
+
+        public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+            super(context, name, factory);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.i("greenDAO", "Upgrading schema from version " + oldVersion + " to " + newVersion + " by dropping all tables");
+            MigrationHelper.migrate(db, UserDao.class);
+        }
     }
 
+//    //输出日志
+//    public void setDebug() {
+//        QueryBuilder.LOG_SQL = true;
+//        QueryBuilder.LOG_VALUES = true;
+//    }
+//
 //    public void close() {
 //        closeHelper();
 //        closeSession();
