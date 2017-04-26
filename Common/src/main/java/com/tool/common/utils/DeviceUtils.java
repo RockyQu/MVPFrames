c4a7aaeab43ea6c18313af5bd2e054bc7d29303e
DesignQu <250533855@qq.com>
2017-04-26 11:41:52 +0800

    无

--- Common/src/main/java/com/tool/common/utils/DeviceUtils.java
+++ Common/src/main/java/com/tool/common/utils/DeviceUtils.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.Settings;
