8c6f41702719bd8c57829ab49c20574a9c3b9359
DesignQu <250533855@qq.com>
2017-08-30 14:31:17 +0800

    核心包版本升级，引入RxJava2，Glide升级4.0

--- Common/src/main/java/com/tool/common/base/BaseApplication.java
+++ Common/src/main/java/com/tool/common/base/BaseApplication.java
@@ -7,25 +7,7 @@ import com.tool.common.di.component.AppComponent;
 import com.tool.common.utils.AppUtils;
 
 /**
- * GitHub:https://github.com/DesignQu/MVPFrames
- * <p>
- * 一、项目涉及的主要框架
- * 1、Dagger2 https://google.github.io/dagger
- * 2、ButterKnife http://jakewharton.github.io/butterknife
- * 3、Retrofit2 https://github.com/square/retrofit
- * 4、Glide https://github.com/bumptech/glide
- * 5、Okhttp https://github.com/square/okhttp
- * 6、AndroidEventBus https://github.com/hehonghui/AndroidEventBus
- * 7、Gson https://github.com/google/gson
- * 8、GreenDAO https://github.com/greenrobot/greenDAO
- * 9、EasyPermissions https://github.com/googlesamples/easypermissions
- * 10、Logg https://github.com/DesignQu/Logg
- * <p>
- * 二、基本使用方法
- * 1、配置自定义Application必须继承BaseApplication，BaseApplication完成Http框架、图片框架、日志管理框架等初始化工作
- * 2、使用Activity、Fragment、ViewHolder、Service、Adapter请继承BaseActivity、BaseFragment、BaseViewHolder、BaseService、BaseAdapter来初始化MVP架构
- * 3、通过Application获取AppComponent里面的对象可直接使用
- * 4、简单功能及页面无需引入MVP
+ * Source https://github.com/DesignQu/MVPFrames
  */
 public abstract class BaseApplication extends Application implements App {
 
