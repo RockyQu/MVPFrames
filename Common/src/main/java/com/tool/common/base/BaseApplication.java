bc5199c5dcbc451a50dda57e72417d127311f08f
DesignQu <250533855@qq.com>
2017-09-09 14:09:38 +0800

    核心包版本升级，引入RxJava2，Glide升级4.0，修复若干BUG

--- Common/src/main/java/com/tool/common/base/BaseApplication.java
+++ Common/src/main/java/com/tool/common/base/BaseApplication.java
@@ -25,13 +25,6 @@ public abstract class BaseApplication extends Application implements App {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-        // 如果声明了独立进程的Service会导致Application执行多次，这里控制只执行默认主进程
-        // 此处为临时修改，如果涉及组件化，多进程开发，这处应去掉
-//        String processName = AppUtils.getProcessName(this);
-//        if (!getPackageName().equals(processName)) {
-//            return;
-//        }
-
         this.delegate = new AppDelegate(base);
         this.delegate.attachBaseContext(base);
     }
