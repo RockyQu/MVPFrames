121328c32e83dc46e1506f8d93c2a492110028de
DesignQu <250533855@qq.com>
2017-09-18 10:46:49 +0800

    核心包版本升级，引入RxJava2，Glide升级4.0，修复若干BUG

--- App/src/main/java/com/frame/mvp/db/DBModule.java
+++ App/src/main/java/com/frame/mvp/db/DBModule.java
@@ -3,6 +3,7 @@ package com.frame.mvp.db;
 import android.app.Application;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.frame.mvp.db.build.DaoMaster;
@@ -13,6 +14,8 @@ import com.tool.common.db.MigrationHelper;
 import com.tool.common.di.scope.ApplicationScope;
 import com.tool.common.utils.DeviceUtils;
 import com.tool.common.utils.ProjectUtils;
+
+import org.greenrobot.greendao.query.QueryBuilder;
 
 import dagger.Module;
 import dagger.Provides;
@@ -26,13 +29,25 @@ public class DBModule {
     // Application
     private Application application;
 
+    // DB存储路径
+    private String dbPath;
+    // DB名称
+    private String dbName;
+
     // 是否加密
     public static final boolean ENCRYPTED = false;
     // 加密密钥，客户端可以自己生成一个特征性动态Key,也可服务器配合传过来一个Key
     public static String KEY;
 
     public DBModule(Application application) {
+        this(application, ProjectUtils.DB, ProjectUtils.PROJECT_NAME);
+    }
+
+    public DBModule(Application application, String dbPath, String dbName) {
         this.application = application;
+
+        this.dbPath = dbPath;
+        this.dbName = dbName;
 
         if (ENCRYPTED) {
             KEY = DeviceUtils.getIMEI(application);
@@ -42,7 +57,10 @@ public class DBModule {
     @ApplicationScope
     @Provides
     public DBOpenHelper provideDevOpenHelper() {
-        return new DBOpenHelper(new DBContextWrapper(application), ProjectUtils.PROJECT_NAME + ".db", null);
+        if (!TextUtils.isEmpty(dbPath)) {
+            return new DBOpenHelper(new DBContextWrapper(application, dbPath), !TextUtils.isEmpty(dbName) ? dbName : "db", null);
+        }
+        return new DBOpenHelper(new DBContextWrapper(application), !TextUtils.isEmpty(dbName) ? dbName : "db", null);
     }
 
     @ApplicationScope
@@ -72,24 +90,4 @@ public class DBModule {
             MigrationHelper.getInstance().migrate(db, UserDao.class);
         }
     }
-
-//    //输出日志
-//    public void setDebug() {
-//        QueryBuilder.LOG_SQL = true;
-//        QueryBuilder.LOG_VALUES = true;
-//    }
-//
-//    public void closeHelper() {
-//        if (helper != null) {
-//            helper.close();
-//            helper = null;
-//        }
-//    }
-//    //关闭session
-//    public void closeSession() {
-//        if (daoSession != null) {
-//            daoSession.clear();
-//            daoSession = null;
-//        }
-//    }
 }
