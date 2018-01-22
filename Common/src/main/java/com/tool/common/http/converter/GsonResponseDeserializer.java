2ef312bcf2c47997bbc0d5aa7cc23b0cfb2708a3
DesignQu <250533855@qq.com>
2018-01-22 17:47:27 +0800

    ResponseEntity 增加 code 字段

--- Common/src/main/java/com/tool/common/http/converter/GsonResponseDeserializer.java
+++ Common/src/main/java/com/tool/common/http/converter/GsonResponseDeserializer.java
@@ -11,12 +11,12 @@ import com.tool.common.http.ResponseEntity;
 import java.lang.reflect.Type;
 
 /**
- * 这里解决因服务器API接口缺少规范化，需实现Gson的JsonDeserializer接口来完成自定义解析
+ * 这里解决因服务器 API 接口缺少规范化，需实现 Gson 的 JsonDeserializer 接口来完成自定义解析
  * 这里有几种情况
- * 1、当返回结果{"code":0,"info":"\u5bc6\u7801\u9519\u8bef","result":-5}
- * result应为JsonObject或JsonArray，但返回的是-5，该类型为基本数据类型，会导致Gson对泛型的解析错误并抛出异常，导致Retrofit2返回到onFailure方法
- * 2、当返回结果{"code":1,"info":"\u767b\u5f55\u6210\u529f","result":{"userid":"00001"}}
- * result为JsonObject或JsonArray，此时为规范数据，直接返回原数据
+ * 1、例如当返回结果 {"code":0,"info":"\u5bc6\u7801\u9519\u8bef","result":-5}
+ * result应为JsonObject或JsonArray，但返回的是-5，该类型为基本数据类型，会导致Gson对泛型的解析错误并抛出异常，导致 Retrofit2 返回到 onFailure 方法
+ * 2、例如当返回结果 {"code":1,"info":"\u767b\u5f55\u6210\u529f","result":{"userid":"00001"}}
+ * 此时为规范数据，直接返回原数据
  */
 public class GsonResponseDeserializer implements JsonDeserializer<ResponseEntity> {
 
