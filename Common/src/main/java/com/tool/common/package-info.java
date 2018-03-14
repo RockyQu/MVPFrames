e40ee16a4e142f400dcb8252f0ee24feaf958c48
DesignQu <250533855@qq.com>
2018-03-14 10:12:11 +0800

    提升 Android Gradle Plugin 到 v3.0.1 提升 Android SDK 到 27 提升 BuildTools 到 v27.0.3 提升 Android Support Library 到 v27.0.2 提升 Dagger 到 v2.14.1 提升 Glide 到 v4.5.0 提升 Rxlifecycle 到 v2.2.1 提升 Okhttp 到 v3.9.1 提升 RxJava 到 v2.1.8 提升 RxCache 到 v1.8.3 提升 RxPermissions 到 v0.9.5 增加 Snacker 效果类似 Snackbar 只是显示在窗口上面，从上往下推出 增加 Toaster 对 Toast 二次封闭，更合理的链式 API 调用方式 移除 ToastBar 增加 BaseSimpleDialogFragment 基于 DialogFragment 可以轻松实现各种 Dialog 效果 完善 文件下载模块，基于 FileDownloader 基本封装，后面会继续完善 增加 ImageLoader 配置参数 ScaleType 用法和 ImageView.ScaleType 属性相同，默认不设置任何ScaleType 完善 PermissionUtils 应用权限管理，使用 RxPermissions 基于 RxJava2 处理Android运行时权限的响应式库 完善 AppDemo

--- Common/src/main/java/com/tool/common/package-info.java
+++ Common/src/main/java/com/tool/common/package-info.java
@@ -17,24 +17,5 @@
 /**
  * 更新日志
  * <p>
- * 提升 Android Gradle Plugin 到 v3.0.1
- * 提升 Android SDK 到 27
- * 提升 BuildTools 到 v27.0.3
- * 提升 Android Support Library 到 v27.0.2
- * 提升 Dagger 到 v2.14.1
- * 提升 Glide 到 v4.5.0
- * 提升 Rxlifecycle 到 v2.2.1
- * 提升 Okhttp 到 v3.9.1
- * 提升 RxJava 到 v2.1.8
- * 提升 RxCache 到 v1.8.3
- * 提升 RxPermissions 到 v0.9.5
- * 增加 Snacker 效果类似 Snackbar 只是显示在窗口上面，从上往下推出
- * 增加 Toaster 对 Toast 二次封闭，更合理的链式 API 调用方式
- * 移除 ToastBar
- * 增加 BaseSimpleDialogFragment 基于 DialogFragment 可以轻松实现各种 Dialog 效果
- * 完善 文件下载模块，基于 FileDownloader 基本封装，后面会继续完善
- * 增加 ImageLoader 配置参数 ScaleType 用法和 ImageView.ScaleType 属性相同，默认不设置任何ScaleType
- * 完善 PermissionUtils 应用权限管理，使用 RxPermissions 基于 RxJava2 处理Android运行时权限的响应式库
- * 完善 AppDemo
  */
 package com.tool.common;
