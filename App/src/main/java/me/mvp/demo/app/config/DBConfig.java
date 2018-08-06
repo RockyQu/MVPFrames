76f1c9b49ea950dca2a8db9dc90affd32d383016
DesignQu <250533855@qq.com>
2018-08-06 10:39:50 +0800

    修改了数据库的一些变量值

--- App/src/main/java/me/mvp/demo/app/config/DBConfig.java
+++ App/src/main/java/me/mvp/demo/app/config/DBConfig.java
@@ -2,9 +2,11 @@ package me.mvp.demo.app.config;
 
 import android.content.Context;
 
+import me.mvp.demo.app.AppConfiguration;
 import me.mvp.demo.db.AppDatabase;
 import me.mvp.frame.db.DatabaseConfig;
 import me.mvp.frame.di.module.DBModule;
+import me.mvp.frame.utils.AppUtils;
 
 /**
  * 扩展自定义配置数据库参数
@@ -15,7 +17,7 @@ public class DBConfig implements DBModule.DBConfiguration {
     @Override
     public void configDB(Context context, DatabaseConfig.Builder builder) {
         builder
-                .name("Frames")
+                .name(AppUtils.getAppChannel(context, AppConfiguration.CHANNEL))
                 .databaseClass(AppDatabase.class)
                 .allowMainThreadQueries()
         ;
